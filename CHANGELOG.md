547f814783ba047e9a9ab550582ca1258d358323
zarmstrong <zarmstrong@traffid.com>
2021-01-10 02:36:50 -0700

    ## 1.0.1 (Jan 10, 2021)
    * Actually works.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1 (Jan 10, 2021)
+
+* Actually works.
+
 ## 1.0.0 (Jan 10, 2021)
 
 * Initial release -- hides the view modules button from users with role < 3 (assistant)
--- slate-hide-buttons.js
+++ slate-hide-buttons.js
@@ -1,4 +1,8 @@
-$(document).ready(function(){
-	if (game.users.current.role < 3)
-	     $("button[data-action=modules]").hide();
-});
+Hooks.on('renderSettings', async () => {
+		console.log(`%c Hide Buttons %c| Loading: user role == ${game.users.current.role}`,'color: #aa0000','color: #ffffff')
+		if (game.users.current.role < 3)
+		{
+			console.log(`%c Hide Buttons %c| Hiding view modules button`,'color: #aa0000','color: #ffffff');
+		    $("button[data-action=modules]").hide();
+		}
+	});
